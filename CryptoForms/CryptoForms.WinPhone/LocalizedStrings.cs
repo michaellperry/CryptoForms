ba0305610a9a531febc16b732877b82f95ff0097
Michael L Perry <michael@qedcode.com>
2015-07-01 09:27:00 -0500

    Xamarin Forms crypto example.

---
+++ CryptoForms/CryptoForms.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using CryptoForms.WinPhone.Resources;
+
+namespace CryptoForms.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ CryptoForms/CryptoForms/MainPage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace CryptoForms
+{
+    public class MainPage : ContentPage
+    {
+
+        private Label label = null;
+        private Button button = null;
+
+        public MainPage()
+        {
+            button = new Button
+            {
+                Text = "Click me",
+            };
+
+            label = new Label
+            {
+                XAlign = TextAlignment.Center,
+                Text = "Welcome to Xamarin Forms!"
+            };
+            button.Clicked += async (sender, e) =>
+            {
+                var data = "Cryptographic example";
+                var pass = "MySecretKey";
+
+                var contentPage = new ContentPage();
+                var salt = Crypto.CreateSalt(16);
+                await contentPage.DisplayAlert("Alert", "Encrypting String " + data + ", with salt " + BitConverter.ToString(salt), "OK");
+                var bytes = Crypto.EncryptAes(data, pass, salt);
+                await contentPage.DisplayAlert("Alert", "Encrypted, Now Decrypting", "OK");
+                var str = Crypto.DecryptAes(bytes, pass, salt);
+                await contentPage.DisplayAlert("Alert", "Decryted " + str, "OK");
+            };
+
+            this.Title = "Crypto Forms";
+            // The root page of your application    
+            this.Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Children =    
+                {    
+                    label,    
+                    button    
+    
+                }
+            };
+
+
+        }
+    }
+}
